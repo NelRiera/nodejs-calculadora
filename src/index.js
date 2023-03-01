dd5abb1744c4b4ded1c089c62915c23f7892f624
NelRiera <manel.s.riera@gmail.com>
2023-03-01 22:45:48 +0100

    Refactor y tratamiento de errores

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
+import { InvalidInputError } from '#Errors/invalidInputError.js';
+import { getOperator } from '#Lib/getOperator.js';
 import { promptQuestion } from '#Lib/promptQuestion.js';
 
 (async () => {
-    // 1º Capturar la entrada del usuario por consola.
-    const userAnswer = await promptQuestion('Introduce tu operación: ');
-    console.log(userAnswer);
+    try {
+        // 1º Capturar la entrada del usuario por consola.
 
-    // 2on Validar l'entrada i separar les part de la mateixa en operands i operador
+        const userAnswer = await promptQuestion('Introduce tu operación: ');
+        console.log(userAnswer);
+
+        // 2on Validar l'entrada i separar les part de la mateixa en operands i operador
+        const standarizeInput = userAnswer.trim();
+
+        if (standarizeInput === '') throw new InvalidInputError();
+
+        const operator = getOperator(standarizeInput);
+
+        if (!operator) throw new InvalidInputError();
+    } catch (error) {
+        if (error instanceof InvalidInputError) console.log(error.message);
+        else
+            console.log(
+                `Error no controlado: ${error.message}. Stack: ${error.stack}`
+            );
+    }
     // 3er Realitar l'operació
     // 4art Mostrar resultat per consola
 })();
---
+++ src/lib/getOperator.js
@@ -0,0 +1,21 @@
+import { OPERATORS } from '#Contants/operators.js';
+import { InvalidInputError } from '#Errors/invalidInputError.js';
+
+export const getOperator = (standarizeInput) => {
+    let operator;
+
+    for (const allowedOperator of OPERATORS) {
+        if (standarizeInput.includes(allowedOperator)) {
+            if (
+                operator ||
+                standarizeInput.indexOf(allowedOperator) !==
+                    standarizeInput.lastIndexOf(allowedOperator)
+            )
+                throw new InvalidInputError();
+
+            operator = allowedOperator;
+        }
+    }
+
+    return operator;
+};
