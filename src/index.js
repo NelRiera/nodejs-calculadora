769db75997171dd97fd6f7e479ebf7a345b82816
NelRiera <manel.s.riera@gmail.com>
2023-03-01 10:41:06 +0100

    Capturar entrada del usuario

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-import { printHello } from '#Lib/hello.js';
+import { promptQuestion } from '#Lib/promptQuestion.js';
 
-printHello();
+(async () => {
+    // 1º Capturar la entrada del usuario por consola.
+    const userAnswer = await promptQuestion('Introduce tu operación: ');
+    console.log(userAnswer);
+
+    // 2on Validar l'entrada i separar les part de la mateixa en operands i operador
+    // 3er Realitar l'operació
+    // 4art Mostrar resultat per consola
+})();
